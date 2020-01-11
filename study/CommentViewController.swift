91d568fc3ae3d49e4dda0c4da55f48a123d164c0
Ma <manakamm4062@gmail.com>
2020-01-11 21:42:17 +0900

    CommentControllerSwiftのところを追加した。 基本はメモ帳のまんま

--- study/CommentViewController.swift
+++ study/CommentViewController.swift
@@ -8,12 +8,54 @@
 
 import UIKit
 
-class CommentViewController: UIViewController {
+class CommentViewController: UIViewController,UITextFieldDelegate {
+    
+    @IBOutlet var nichijiLabel: UITextField!
+    @IBOutlet var jikanLabel: UITextField!
+    @IBOutlet var contentTextView: UITextView!
+    
+    var saveDate: UserDefaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        nichijiLabel.text = saveDate.object(forKey: "title") as? String
+        jikanLabel.text = saveDate.object(forKey: "title2") as? String
+        contentTextView.text = saveDate.object(forKey: "content") as? String
+        nichijiLabel.delegate = self
+        jikanLabel.delegate = self
 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveMemo() {
+        
+        //UserDefaultsに書き込み
+        saveDate.set(nichijiLabel.text, forKey: "title")
+        saveDate.set(jikanLabel.text, forKey: "title2")
+        saveDate.set(contentTextView.text, forKey: "content")
+        
+        //alertを出す
+        let alert: UIAlertController = UIAlertController(title: "保存", message: "メモの保存が完了しました。", preferredStyle: .alert)
+        
+        //OKボタン
+        alert.addAction(
+                        UIAlertAction(
+                            title: "OK",
+                            style: .default,
+                            handler: { action in
+                                //ボタンが押された時の動作
+                                self.navigationController?.popViewController(animated: true)
+                                print("OKボタンが押されました！")
+                                }
+        )
+        )
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
     
 
--- study/ShowViewController.swift
+++ study/ShowViewController.swift
@@ -12,6 +12,8 @@ class ShowViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
 
         // Do any additional setup after loading the view.
     }
