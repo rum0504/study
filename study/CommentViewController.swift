164fde64717fb4de38633a9c7b964089b746b4f4
Ma <manakamm4062@gmail.com>
2020-01-11 19:29:34 +0900

    Initial Commit

---
+++ study/CommentViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CommentViewController.swift
+//  study
+//
+//  Created by 齊藤愛華 on 2020/01/11.
+//  Copyright © 2020 com.lifeistech. All rights reserved.
+//
+
+import UIKit
+
+class CommentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ study/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  study
+//
+//  Created by 齊藤愛華 on 2020/01/11.
+//  Copyright © 2020 com.lifeistech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func kiroku() {
+        
+    }
+    
+    @IBAction func miru() {
+        
+    }
+    
+    
+    
+    
+
+}
+
