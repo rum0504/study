8fa4b26389ee8c1327a093e9c9dd46856c21e645
Ma <manakamm4062@gmail.com>
2020-01-25 20:10:28 +0900

    画面遷移のところまで 残りは保存のところ！！

--- study/ShowViewController.swift
+++ study/ShowViewController.swift
@@ -8,14 +8,49 @@
 
 import UIKit
 
-class ShowViewController: UIViewController {
+class ShowViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    //storyboardで扱うTableViewを宣言
+    @IBOutlet var table: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //TableViewのデータソースメソッドはViewControllerクラスに描くよという設定
+        table.dataSource = self
+        
+        //TableViewのdelegateメソッドはViewControllerメソッドに書くよという設定
+        table.delegate = self
+        
         
 
         // Do any additional setup after loading the view.
+    }
+    
+    //セルの数を設定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    //ID付きのセルを取得してセル付属のテキストラベルに「テスト」と表示させる
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        cell?.textLabel?.text = "テスト"
+        
+        return cell!
+    }
+    
+    //セルが押された時に呼ばれるメソッド
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("(\(indexPath.row))番目の行が選択されました")
+        
+        //セルの選択を解除
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        //別の画面に遷移
+        performSegue(withIdentifier: "toComment", sender: indexPath.row)
     }
     
 
